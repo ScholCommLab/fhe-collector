1ef292cd76d07a4cefbfcf9f68607031bcc912fd
Stefan Kasberger <mail@stefankasberger.at>
2018-03-21 11:29:44 +0100

    update import_csv command

--- app/models.py
+++ app/models.py
@@ -7,6 +7,14 @@ class Publication(db.Model):
     url = db.Column(db.String(128))
     doi = db.Column(db.String(64), index=True)
     date = db.Column(db.Date())
+    doi_url = db.Column(db.String(128))
+    doi_resolve_ts = db.Column(db.Date())
+    doi_resolve_status = db.Column(db.String(128))
+    doi_resolve_error = db.Column(db.String(128))
+    pmid = db.Column(db.String(128))
+    pmc = db.Column(db.String(128))
+    ncbi_ts = db.Column(db.Date())
+    ncbi_errmsg = db.Column(db.String(128))
 
     def __init__(self, url, doi, date):
         self.url = url
--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ import datetime
 import time
 import sys
 import json
+import requests
 from dateutil.parser import parse
 from random import shuffle
 from pathlib import Path
@@ -13,7 +14,6 @@ from flask_script import Manager, Server, Command, prompt_bool
 from flask_script.commands import ShowUrls, Clean
 from app import create_app
 from app.models import db, Publication
-
 
 
 app = create_app()
@@ -76,6 +76,8 @@ class ImportCsv(Command):
     #     return False
 
     def run(self):
+        df_testsize = 5
+
         # import csv to pandas dataframe
         df = pd.read_csv('app/static/data/PKP_20171220_100.csv', encoding='utf8', parse_dates=True)
         df = df.drop_duplicates()
@@ -97,16 +99,109 @@ class ImportCsv(Command):
                     if re.match(pat, doi, re.IGNORECASE):
                         valid_dois.append(True)
         df = df[valid_dois]
-        df = df.drop_duplicates()
+        df = df.drop_duplicates().set_index('doi')
+        df = df[:df_testsize]
+
+        # request data from ncbi
+        url_base = 'https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0'
+        batchsize = 200
+        df['pmid'] = None
+        df['pmc'] = None
+        df['ncbi_ts'] = None
+        df['ncbi_errmsg'] = None
+        params = {
+            'email': 'aenkhbay@sfu.ca',
+            'tool': 'ScholCommLab ID Crawler - scholcommlab.ca',
+            'idtype': 'doi',
+            'versions': 'no',
+            'format': 'json'
+        }
+        dois = list(set(df.index.tolist()))
+        batches = range(0, len(dois), batchsize)
+        for batch_start in batches:
+            now = datetime.datetime.now()
+            batch = dois[batch_start:batch_start + batchsize]  # the result might be shorter than batchsize at the end
+            params['ids'] = ",".join(batch)
+            response = requests.get(url_base, params=params)
+            records = json.loads(response.text)['records']
+
+            for record in records:
+                doi = record['doi']
+                df.loc[doi, 'ncbi_ts'] = now
+
+                if 'pmid' in record.keys():
+                    df.loc[doi, 'pmid'] = record['pmid']
+
+                if 'pmcid' in record.keys():
+                    df.loc[doi, 'pmc'] = record['pmcid'][3:]
+
+                if 'errmsg' in record.keys():
+                    df.loc[doi, 'ncbi_errmsg'] = record['errmsg']
+
+        # resolve doi's
+        df['doi_url'] = None
+        df['doi_resolve_ts'] = None
+        df['doi_resolve_status'] = None
+        df['doi_resolve_error'] = None
+        timeout = 5
+
+        for doi in df.index:
+            now = datetime.datetime.now()
+
+            # Init row values
+            doi_resolve_status = None
+            doi_resolve_error = None
+            doi_url = None
+
+            # Resolve DOI
+            try:
+                response = requests.get('https://doi.org/{}'.format(doi), allow_redirects=True, timeout=timeout)
+                if response.ok:
+                    doi_resolve_status = response.status_code
+                    doi_url = response.url
+                else:
+                    response.urdoi_resolve_status = response.status_code
+                    doi_resolve_error = response.reason
+            except requests.exceptions.Timeout as ex:
+                doi_resolve_error = "Timeout"
+            except requests.exceptions.TooManyRedirects as ex:
+                doi_resolve_error = "TooManyRedirects"
+            except requests.exceptions.RequestException as ex:
+                doi_resolve_error = "RequestException"
+
+            df.loc[doi, 'doi_url'] = doi_url
+            df.loc[doi, 'doi_resolve_status'] = doi_resolve_status
+            df.loc[doi, 'doi_resolve_error'] = doi_resolve_error
+            df.loc[doi, 'doi_resolve_ts'] = now
+        print(df[:5])
+
+        # save data to database
+        num_added = 0
+        num_already_in = 0
         for index, row in df.iterrows():
-            result = Publication.query.filter_by(url=row['url'], doi=row['doi']).first()
+            print(index)
+            result = Publication.query.filter_by(url=row['url'], doi=index).first()
             if result is None:
-                pub = Publication(url=row['url'], doi=row['doi'], date=row['date'])
+                pub = Publication(
+                    url=row['url'],
+                    doi=row['doi'],
+                    date=row['date'],
+                    pmid=row['pmid'],
+                    pmc=row['pmc'],
+                    ncbi_ts=row['ncbi_ts'],
+                    ncbi_errmsg=row['ncbi_errmsg'],
+                    doi_url=row['doi_url'],
+                    doi_resolve_status=row['doi_resolve_status'],
+                    doi_resolve_error=row['doi_resolve_error'],
+                    doi_resolve_ts=row['doi_resolve_ts']
+                )
                 db.session.add(pub)
-                print('Add new publication to database.')
+                num_added += 1
             else:
-                print('Publication already in database.')
+                num_already_in += 1
         db.session.commit()
+        print(num_added, ' publications added to database.')
+        print(num_already_in, ' publications already in database.')
 
 
 class FacebookRequest(Command):
