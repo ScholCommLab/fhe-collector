2fdfd63512e2b929501a354402cdf337e3cf7185
Stefan Kasberger <mail@stefankasberger.at>
2018-04-30 10:08:19 +0200

    update models.py

--- app/models.py
+++ app/models.py
@@ -1,46 +1,65 @@
 from app import db
-import datetime
+# import datetime
 
 
 class Publication(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(128))
-    doi = db.Column(db.String(64), index=True)
-    date = db.Column(db.Date())
-    doi_url = db.Column(db.String(128))
-    doi_resolve_ts = db.Column(db.Date())
-    doi_resolve_status = db.Column(db.String(128))
-    doi_resolve_error = db.Column(db.String(128))
-    pmid = db.Column(db.String(128))
-    pmc = db.Column(db.String(128))
-    ncbi_ts = db.Column(db.Date())
-    ncbi_errmsg = db.Column(db.String(128))
+    doi = db.Column(db.String(64), primary_key=True)
+    pub_date = db.Column(db.DateTime())
+    origin = db.Column(db.String(256))  # 'import FILENAME' or 'api'
+    date_added = db.Column(db.DateTime())
 
-    def __init__(self, url, doi, date):
-        self.url = url
-        self.doi = doi
-        self.date = datetime.datetime.strptime(date, '%Y-%m-%d')
+    # def __init__(self, url, doi, date):
+    #     self.date = datetime.datetime.strptime(date, '%Y-%m-%d')
 
     def __repr__(self):
         return '<Publication {}>'.format(self.doi)
 
 
-class FacebookResponse(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class Url(db.Model):
+    url = db.Column(db.String(256), primary_key=True)
+    publication_doi = db.Column(db.String(64), db.ForeignKey('publication.doi'), nullable=False)
+    url_type = db.Column(db.String(64))  # pubmedcentral, pubmed, doi.org,
+    origin = db.Column(db.String(256))  # ncbi, doi.org, 'import FILENAME' or 'api'
+    date_added = db.Column(db.DateTime())
 
     def __repr__(self):
-        return '<Facebook Response {}>'.format(self.id)
+        return '<URL {}>'.format(self.url)
 
 
-class AltmetricsResponse(db.Model):
+class FBRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    url_url = db.Column(db.String(256), db.ForeignKey('url.url'), nullable=False)
+    request = db.Column(db.Text())
+    response = db.Column(db.Text())
+    engagement = db.Column(db.Text())
+    timestamp = db.Column(db.DateTime())
+    error = db.Column(db.Boolean())
 
     def __repr__(self):
-        return '<Altmetrics Response {}>'.format(self.id)
+        return '<Facebook Request {}>'.format(self.id)
 
 
-class CrossRefResponse(db.Model):
+class AltmetricsRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    publication_doi = db.Column(db.String(64), db.ForeignKey('publication.doi'), nullable=False)
+    request = db.Column(db.Text())
+    response = db.Column(db.Text())
+    timestamp = db.Column(db.DateTime())
+    error = db.Column(db.Boolean())
 
     def __repr__(self):
-        return '<CrossRef Response {}>'.format(self.id)
+        return '<Altmetrics Request {}>'.format(self.id)
+
+
+class NCBIRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url_url = db.Column(db.String(256), db.ForeignKey('url.url'), nullable=False)
+    request = db.Column(db.Text())
+    response = db.Column(db.Text())
+    pmcid = db.Column(db.String(64))
+    pmid = db.Column(db.String(64))
+    timestamp = db.Column(db.DateTime())
+    error = db.Column(db.Boolean())
+
+    def __repr__(self):
+        return '<NCBI Request {}>'.format(self.id)
