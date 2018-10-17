09e7b73106984195978c328c0940b58a4896f1ae
Stefan Kasberger <mail@stefankasberger.at>
2018-10-17 17:37:10 +0200

    update settings files

--- settings_default.py
+++ settings_default.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     """
-
+    Setting the default environment settings.
     """
     DEBUG = False
     TESTING = False
@@ -14,7 +14,8 @@ class Config(object):
 
 class Development(Config):
     """
-
+    Setting the development environment settings.
+    Database is sqlite file or a postgresql database string passed by an environment variable.
     """
     DEBUG = True
     DEBUG_TB_INTERCEPT_REDIRECTS = False
@@ -27,21 +28,26 @@ class Development(Config):
 
 class Testing(Config):
     """
-
+    Setting the test environment settings.
     """
     TESTING = True
     SQLALCHEMY_ECHO = True
+    # only execute if code is running on travis
     if 'TRAVIS' in os.environ:
 
         if 'SECRET_KEY' in os.environ:
             SECRET_KEY = os.getenv('SECRET_KEY')
         else:
             print('SECRET_KEY is missing.')
+    if 'SQLALCHEMY_DATABASE_URI' in os.environ:
+        SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
 
 
 class Production(Config):
     """
-
+    Setting the production environment settings.
     """
     USER_SETTINGS_EXIST = True
     if 'SQLALCHEMY_DATABASE_URI' in os.environ:
