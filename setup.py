b00482d3e9280dd83e61fb8f353f982d3b12a419
skasberger <mail@stefankasberger.at>
2019-10-22 01:00:32 +0200

    fix issue urllib python 2.7 error try #11

--- setup.py
+++ setup.py
@@ -81,7 +81,6 @@ CLASSIFIERS = [
     'Operating System :: OS Independent',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
