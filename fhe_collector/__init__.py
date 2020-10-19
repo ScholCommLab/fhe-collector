e204bea5a98b50cc8d7f8b5054b11ac83d2e99ab
skasberger <mail@stefankasberger.at>
2020-10-19 12:50:29 +0200

    Fix issue: Travis setting

--- fhe_collector/__init__.py
+++ fhe_collector/__init__.py
@@ -40,20 +40,24 @@ def create_app(test_config=None):
     if "SECRET_KEY" in os.environ:
         app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
+    if "TRAVIS" in os.environ:
+        app.config["TRAVIS"] = os.getenv("TRAVIS")
+    else:
+        app.config["TRAVIS"] = False
+
     # Load test_config dict()
     if test_config is not None:
         app.config.update(test_config)
         print("* Settings create_app() #1: Loaded")
         print("TESTING" in app.config)
         print(app.config["TESTING"])
+        print("TRAVIS" in os.environ)
 
     # Load instance specific default settings and setup instance
     if app.config["TESTING"]:
         print("IN: #1")
-        is_travis = os.getenv("TRAVIS", default=False)
-        if is_travis:
+        if app.config["TRAVIS"]:
             print("IN: #2")
-            app.config["TRAVIS"] = is_travis
             app.config.from_object("fhe_collector.settings_default.Travis")
         else:
             print("IN: #3")
